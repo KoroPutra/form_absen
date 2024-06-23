2f60b78d629e8e678591f9797c78d79241d8f450
PutraMuhammadFerdiansyah <koroputra000@gmail.com>
2024-06-23 23:58:20 +0700

    Log: Make create

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Absen;
 
 class AbsenController extends Controller
 {
@@ -13,6 +14,22 @@ class AbsenController extends Controller
 
     function submit(Request $request)
     {
-        
+        $request->validate([
+            'nip' => 'required',
+            'tgl_masuk' => 'required|date',
+            'jam_absen_manual' => 'required|date_format:H:i',
+            'jenis_kehadiran' => 'required',
+            'status_kehadiran' => 'required',
+        ]);
+
+        $absen = new Absen;
+        $absen->nip = $request->nip;
+        $absen->tgl_masuk = $request->tgl_masuk;
+        $absen->jam_absen_manual = $request->tgl_masuk . ' ' . $request->jam_absen_manual . ':00';
+        $absen->jenis_kehadiran = $request->jenis_kehadiran;
+        $absen->status_kehadiran = $request->status_kehadiran;
+        $absen->save();
+
+        return redirect()->route('absen.index')->with('success', 'Absen berhasil disimpan.');
     }
 }
--- app/Models/Absen.php
+++ app/Models/Absen.php
@@ -10,4 +10,10 @@ class Absen extends Model
     use HasFactory;
 
     protected $table = 'absen';
+
+    protected $fillable = [
+        'nip', 'tgl_masuk', 'jam_absen_manual', 'jenis_kehadiran', 'status_kehadiran',
+    ];
+
+    public $timestamps = false;
 }
